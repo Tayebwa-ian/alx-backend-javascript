90a8ef8471e3fbeba15ea4485a62e37e49c07716
Tayebwa-ian <tayebwaian0@gmail.com>
2024-05-29 23:27:59 +0300

    more promises

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -4,12 +4,10 @@ export default function handleResponseFromAPI(promise) {
       console.log('Got a response from the API');
       return {
         status: 200,
-        body: 'Success',
+        body: 'success',
       };
-    },
-    () => {
+    }).catch(() => {
       console.log('Got a response from the API');
-      throw Error();
-    },
-  );
+      throw Error("empty");
+    });
 }
