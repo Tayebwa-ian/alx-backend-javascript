1396ecdf4ca5c8a419e926ff34cf01828afd4a5a
Tayebwa-ian <tayebwaian0@gmail.com>
2024-06-08 00:11:35 +0200

    creating an array with objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,20 @@
+export default function getListStudents() {
+  const arrayObjects = [
+    {
+      id: '1',
+      firstname: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: '2',
+      firstname: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: '5',
+      firstname: 'Serana',
+      location: 'San Francisco',
+    },
+  ];
+  return arrayObjects;
+}
