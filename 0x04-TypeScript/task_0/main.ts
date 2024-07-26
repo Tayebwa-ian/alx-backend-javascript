217fd919210e4544183dac0fa4c5473e1c7281b9
Tayebwa-ian <tayebwaian0@gmail.com>
2024-07-26 19:28:45 +0200

    JS tests

---
+++ 0x04-TypeScript/task_0/main.ts
@@ -0,0 +1,17 @@
+interface Student {
+    firstName: string;
+    LastName: string;
+    age: number;
+}
+
+const student1: Student = {
+    firstName: "derrick",
+    LastName: "Taremwa",
+    age: 29,
+};
+
+const student2: Student = {
+    firstName: "Mark",
+    LastName: "Oyeta",
+    age: 32,
+};
